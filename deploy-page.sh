8ca82cb20bb1ee3bb1da9d38e1bbaa762d23890e
Laurent Christophe <laurent.christophe.terken@gmail.com>
2024-09-30 15:01:15 +0200

    doc: github pages index

--- deploy-page.sh
+++ deploy-page.sh
@@ -5,6 +5,8 @@ git branch -D gh-pages
 git checkout --orphan gh-pages
 git reset
 echo "disable jekyll" > .nojekyll
+git add .nojekyll
+git add index.html
 git add -f typedoc
 git commit -m "deploy to GitHub pages"
 git push -f origin gh-pages
